c679c960b6e7c26ac73898c473a7279cbe14a2a5
JessonChan <chinachendongpo@gmail.com>
2020-02-14 11:14:44 +0800

    time bug fixed

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func main() {
 		RegTplFunc("str2html", func(s string) template.HTML { return template.HTML(s) }).
 		RegTplFunc("date", func(t time.Time, f string) string {
 			if f == "Y-m-d H:i:s" {
-				return t.Format("2006-03-02 15:04:05")
+				return t.Format("2006-01-02 15:04:05")
 			} else {
-				return t.Format("2006-03-02")
+				return t.Format("2006-01-02")
 			}
 		}).
 		Run(cango.Addr{Port: 8099},
