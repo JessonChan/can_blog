df3b9f1eff23f4e1741e157928d20171f80341b6
JessonChan <chinachendongpo@gmail.com>
2020-02-24 20:22:14 +0800

    clean cod

--- filter/visit.go
+++ filter/visit.go
@@ -18,6 +18,6 @@ type VisitFilter struct {
 var _ = cango.RegisterFilter(&VisitFilter{})
 
 func (v *VisitFilter) PreHandle(req *http.Request) interface{} {
-	canlog.CanDebug(req.Method, req.URL.Path, req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("User-Agent"), req.Header)
+	canlog.CanDebug(req.Method, req.URL.Path, req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("User-Agent"))
 	return true
 }
