d351e78d11fc29d984cbe70a2520ffe1f9e0bef0
JessonChan <chinachendongpo@gmail.com>
2020-02-25 23:20:09 +0800

    使用最新的注册方式

--- filter/visit.go
+++ filter/visit.go
@@ -14,6 +14,6 @@ type VisitFilter struct {
 var _ = cango.RegisterFilter(&VisitFilter{})
 
 func (v *VisitFilter) PreHandle(req *http.Request) interface{} {
-	canlog.CanDebug(req.Method, req.URL.Path, req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("User-Agent"))
+	canlog.CanDebug(req.Method, req.URL.Path, req.RemoteAddr, req.Header.Get("X-Forwarded-For"))
 	return true
 }
